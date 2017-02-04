79d2650608a8e4516a8bb09f268cca43eead069c
robert kermode <kermoder@gmail.com>
2017-02-04 13:50:28 -0500

    substantial simplifications

--- src/rest/restHandlers.go
+++ src/rest/restHandlers.go
@@ -2,47 +2,57 @@ package rest
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"reflect"
 )
 
-type RestHandler interface {
-	Handler(http.ResponseWriter, *http.Request)
-	ServeRest(params map[string][]string) (interface{}, *RestError)
-	Context() string
+type APIProvider func() HandlerSpec
+
+type RestHandler func(map[string][]string) (interface{}, *RestError)
+
+type HandlerSpec struct {
+	Context   string
+	ServeRest RestHandler
 }
 
-type RestHandlerBase struct {
-	context string
-	rh      RestHandler
-}
+type HttpHandler func(http.ResponseWriter, *http.Request)
 
-func NewRestHandlerBase(context string, rh RestHandler) *RestHandlerBase {
-	rhb := new(RestHandlerBase)
-	rhb.context = context
-	rhb.rh = rh
-	return rhb
-}
-
-func (rhb *RestHandlerBase) Handler(rw http.ResponseWriter, r *http.Request) {
-	e0 := r.ParseForm()
-	if e0 != nil {
-		io.WriteString(rw, ErrorMessages[REQUEST_PARSE])
-		return
+func MuxHandler(hs HandlerSpec) HttpHandler {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		e0 := r.ParseForm()
+		if e0 != nil {
+			io.WriteString(rw, ErrorMessages[REQUEST_PARSE])
+			return
+		}
+		content, e1 := hs.ServeRest(r.Form)
+		if e1 != nil {
+			io.WriteString(rw, ErrorMessages[GENERIC_SERVER])
+			return
+		}
+		byteArr, e2 := json.Marshal(content)
+		if e2 != nil {
+			io.WriteString(rw, ErrorMessages[JSON_MARSHALLING])
+			return
+		}
+		rw.Header().Add("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		rw.Write(byteArr)
 	}
-	content, e1 := rhb.rh.ServeRest(r.Form)
-	if e1 != nil {
-		io.WriteString(rw, ErrorMessages[GENERIC_SERVER])
-		return
-	}
-	byteArr, e2 := json.Marshal(content)
-	if e2 != nil {
-		io.WriteString(rw, ErrorMessages[JSON_MARSHALLING])
-		return
-	}
-	rw.Write(byteArr)
 }
 
-func (rhb *RestHandlerBase) Context() string {
-	return rhb.context
+func AcceptRequests(restAPI interface{}) {
+	mux := http.NewServeMux()
+	raValue := reflect.ValueOf(restAPI)
+	for i := 0; i < raValue.NumMethod(); i++ {
+		spec := raValue.Method(i).Call([]reflect.Value{})
+		specIF := spec[0].Interface()
+		hs := specIF.(HandlerSpec)
+		handler := MuxHandler(hs)
+		fmt.Println("handling ", hs.Context)
+		mux.HandleFunc(hs.Context, handler)
+	}
+	fmt.Println("Listening...")
+	http.ListenAndServe(":9090", mux)
 }
