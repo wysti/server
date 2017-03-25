17ac5435aff19388710645027880147f9aedf1e7
robert kermode <kermoder@gmail.com>
2017-03-25 14:38:18 -0400

    added a comment

--- src/rest/restHandlers.go
+++ src/rest/restHandlers.go
@@ -21,27 +21,33 @@ type HttpHandler func(http.ResponseWriter, *http.Request)
 
 func MuxHandler(hs HandlerSpec) HttpHandler {
 	return func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
 		e0 := r.ParseForm()
 		if e0 != nil {
 			io.WriteString(rw, ErrorMessages[REQUEST_PARSE])
+			rw.WriteHeader(http.StatusBadRequest)
 			return
 		}
 		content, e1 := hs.ServeRest(r.Form)
 		if e1 != nil {
 			io.WriteString(rw, ErrorMessages[GENERIC_SERVER])
+			rw.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		byteArr, e2 := json.Marshal(content)
 		if e2 != nil {
 			io.WriteString(rw, ErrorMessages[JSON_MARSHALLING])
+			rw.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		rw.Header().Add("Content-Type", "application/json")
 		rw.WriteHeader(http.StatusOK)
 		rw.Write(byteArr)
 	}
 }
 
+/**
+ * The methods of restAPI  should all effect MuxHandler, defined just above
+ */
 func AcceptRequests(restAPI interface{}) {
 	mux := http.NewServeMux()
 	raValue := reflect.ValueOf(restAPI)
@@ -50,7 +56,7 @@ func AcceptRequests(restAPI interface{}) {
 		specIF := spec[0].Interface()
 		hs := specIF.(HandlerSpec)
 		handler := MuxHandler(hs)
-		fmt.Println("handling ", hs.Context)
+		fmt.Println("handling", hs.Context)
 		mux.HandleFunc(hs.Context, handler)
 	}
 	fmt.Println("Listening...")
